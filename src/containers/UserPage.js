4f30099eea6eafe5a962bb9865d15fc10cae727b
conghau <conghau.master1@gmail.com>
2018-06-30 14:13:46 +0700

    Add lint-staged

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,20 +1,20 @@
 /**
  * Created by hautruong on 6/30/18.
  */
-import React,  {Component} from 'react';
+import React, { Component } from 'react';
 
 class UserPage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="__user_page">
-                <p>UserPage Page</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="__user_page">
+        <p>UserPage Page</p>
+      </div>
+    );
+  }
 }
 
 export default UserPage;
